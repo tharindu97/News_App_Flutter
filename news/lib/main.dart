5aee315c9a36a54db57c4ff7e816b1bbee51245d
tharindu97 <tharindukavishna@gmail.com>
2020-09-02 18:47:09 +0530

    diplay_news

--- news/lib/main.dart
+++ news/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class News{
 
@@ -23,6 +25,7 @@ class News{
   );
 
   News.fromJson(Map<String, dynamic> json){
+
     id = json['id'];
     url = json['url'];
     title = json['title'];
@@ -31,6 +34,7 @@ class News{
     author = json['author'];
     image = json['image'];
     date = json['date'];
+    
   }
 
 }
@@ -61,9 +65,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  static List<News> _news = List<News>();
+  static List<News> _newsInApp = List<News>();
+
+  Future<List<News>> comingNews() async{
+
+    var url = 'http://www.mocky.io/v2/5ecfddf13200006600e3d6d0';
+    var response = await http.get(url);
+    var news = List<News>();
+
+    if(response.statusCode == 200){
+      var notesJson = json.decode(response.body);
+      for(var noteJson in notesJson){
+        news.add(News.fromJson(noteJson));
+      }
+    }
+    return news;
+  }
+  
+  @override
+  void initState() {
+    comingNews().then((value) {
+      setState(() {
+        _news.addAll(value);
+        _newsInApp = _news;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(97),
         child: Column(
@@ -92,6 +127,73 @@ class _HomePageState extends State<HomePage> {
             )
           ],
         ),
+      ),
+
+      body: ListView.builder(
+        itemBuilder: (context, index){
+          return _listItem(index);
+        },
+        itemCount: _newsInApp.length,
+      ),
+    );
+  }
+
+  _listItem(index){
+    return Container(
+     child: Padding(
+       padding: const EdgeInsets.only(left: 15, top: 1, right: 1, bottom: 1),
+       child: Column(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: <Widget>[
+           Row(
+             mainAxisAlignment: MainAxisAlignment.end,
+             children: <Widget>[
+               Expanded(
+                 child: Padding(
+                   padding: const EdgeInsets.only(bottom: 15),
+                   child: Text(_newsInApp[index].title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                    ),
+                   ),
+                )
+              ),
+
+              Container(
+                height: 50,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: IconButton(
+                    iconSize: 16,
+                    color: Colors.black26,
+                    alignment: Alignment.bottomCenter,
+                    icon: Icon(Icons.arrow_forward_ios), 
+                    onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) {
+
+                    }))
+                  ),
+                ),
+              )
+             ],
+           ),
+           Text(
+             _newsInApp[index].publisher,
+             style: TextStyle(
+               color: Colors.grey.shade600,
+               fontSize: 17,
+             ),
+           ),
+           SizedBox(
+             height: 10,
+           ),
+           Padding(
+             padding: const EdgeInsets.only(bottom: 5),
+             child: Divider(color: Colors.black12,),
+          )
+         ],
+       ),
       ),
     );
   }
